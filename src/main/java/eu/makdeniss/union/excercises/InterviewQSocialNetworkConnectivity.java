7a301c880bc9c665bbd622a823d435e70c8559be
deniss.makarenkov <makdeniss@gmail.com>
2016-10-09 17:52:39 +0300

    Interview Question: Social Network Connectivity

---
+++ src/main/java/eu/makdeniss/union/excercises/InterviewQSocialNetworkConnectivity.java
@@ -0,0 +1,38 @@
+package eu.makdeniss.union.excercises;
+
+import eu.makdeniss.StdIn;
+import eu.makdeniss.StdOut;
+import eu.makdeniss.union.WeightedQuickUnionUF;
+
+/**
+ * Created by Deniss Makarenkov on 09/10/16.
+ * Visit http://www.makdeniss.eu/
+ */
+public class InterviewQSocialNetworkConnectivity {
+
+    public static void main(String[] args) {
+
+        int n = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        String date, time;
+        //timestamps are sorted ascending
+        while (!StdIn.isEmpty()) {
+
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            date = StdIn.readString();
+            time = StdIn.readString();
+
+
+            uf.union(p, q);
+
+            StdOut.println("[" + p + "," + q + "]");
+
+            if (uf.count() == 1) {
+                StdOut.println("All members were connected at: " + date + time);
+                break;
+            }
+
+        }
+    }
+}
